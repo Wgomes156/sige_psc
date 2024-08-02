342d42c64609786f6e557068fda20b3c2dc1eed8
Wgomes156 <wagnergomes156@gmail.com>
2024-08-02 20:37:50 -0300

    renomear projeto para sige_psc

--- sige_psc/asgi.py
+++ sige_psc/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for datapsc project.
+ASGI config for sige_psc project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'datapsc.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sige_psc.settings')
 
 application = get_asgi_application()
--- sige_psc/urls.py
+++ sige_psc/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for datapsc project.
+URL configuration for sige_psc project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
