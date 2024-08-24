231a6fed6d142c8d70312b9dfc3690b92f0530c5
Wgomes156 <wagnergomes156@gmail.com>
2024-08-23 21:22:35 -0300

    Atualizacao Menu

--- sige_psc/settings.py
+++ sige_psc/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-1@4uq&&vb2g!3^v_r1amnuf45b6ybhx48ept6h7f&cp)2&g(wp
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
 
 # Application definition
 
