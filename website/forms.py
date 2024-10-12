93213a9fe6d1129697762c849812a5271f20a44d
Wgomes156 <wagnergomes156@gmail.com>
2024-10-11 21:30:54 -0300

    Atualização acesso usuario

--- website/forms.py
+++ website/forms.py
@@ -1,11 +1,24 @@
 from django import forms
 from .models import User
+class DateInput(forms.DateInput):
+    input_type="date"
+
 
 class UserForm (forms.ModelForm):
+    password =forms.CharField(widget=forms.PasswordInput())
     class Meta: 
         model = User
         fields = [
+            "username",
+            "password",
             "first_name",
             "last_name",
             "email",
+            "telefone",
+            "data_nasc",
+            "dados_pix",
+            "cpf",
         ]
+        widgets={
+            "data_nasc": DateInput()
+        }
