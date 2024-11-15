1e83bca2172a579450896b4bea1b6f9b78b7532f
Wgomes156 <wagnergomes156@gmail.com>
2024-11-14 21:56:42 -0300

    Atualizacao Menu foto cadastro

--- sige_psc/settings.py
+++ sige_psc/settings.py
@@ -99,7 +99,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
+LOGIN_URL="/login"
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
