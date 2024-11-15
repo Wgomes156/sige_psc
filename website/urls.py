ffdd743ef170bf766563338caa1f72ed0b6aede9
Wgomes156 <wagnergomes156@gmail.com>
2024-11-14 21:07:01 -0300

    Nova alteração menu e inclusao foto

--- website/urls.py
+++ website/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from website import views
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name="index"),
     path("login", views.loginview),
     path("cliente", views.create_user, name="criar-usuario"),
     path("cadastros", views.create_user_adm),
-
-]
+    
+] + static("Foto_cadastro", document_root="Foto_cadastro")
 
