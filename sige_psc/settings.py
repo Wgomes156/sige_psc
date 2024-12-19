07af28522936c3f1c5756673e71a94bf7e6c1271
Wgomes156 <wagnergomes156@gmail.com>
2024-12-19 20:29:47 -0300

    Ajuste configuração static files

--- sige_psc/settings.py
+++ sige_psc/settings.py
@@ -117,10 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS =[
-    BASE_DIR/"static"
-]
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT= "/var/www/testeaws.pscservice.site/static/"
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
